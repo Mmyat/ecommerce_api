4e3895e4ded6fce23d55f02eeee2b659e5355cf7
Mmyat <mmyathaha@gmail.com>
2023-10-23 16:56:47 +0630

    2st commit

--- routes/products-route.js
+++ routes/products-route.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const Products = require('./products-route')
 router.use(express.json())
 //http://localhost:8080
 router.get('/',(req,res)=>{
     res.json({message:"Hello Requester"})
+})
+router.post('/post',async(req,res,next)=>{
+    try {
+        const {code,name,price} = req.body;
+        const products = new Products({code,name,price})
+        products.save();
+        res.status(201).json({message:"Created Sucessfully"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status : 500,
+            message : error.message,
+        })
+    }
 })
 module.exports = router;
